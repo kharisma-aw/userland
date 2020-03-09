0300b3ff42a0e07102ddd32a2300a08afac0eaf7
kharisma-aw <aw.kharisma@gmail.com>
2020-03-09 18:42:19 +0700

    initial project commit

---
+++ src/main/kotlin/com/onboarding/userland/UserlandApplication.kt
@@ -0,0 +1,25 @@
+package com.onboarding.userland
+
+import com.onboarding.userland.service.AuthenticationService
+import com.onboarding.userland.service.UserDetailService
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = [SecurityAutoConfiguration::class])
+class UserlandApplication {
+	@Bean
+	fun passwordEncoder(): PasswordEncoder {
+		return BCryptPasswordEncoder()
+	}
+}
+
+fun main(args: Array<String>) {
+	runApplication<UserlandApplication>(*args)
+}
---
+++ src/test/kotlin/com/onboarding/userland/UserlandApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.onboarding.userland
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserlandApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
