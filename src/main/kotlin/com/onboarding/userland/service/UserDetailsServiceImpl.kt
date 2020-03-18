fefa537351bb7cd56c1d70ebb6a0d12cd3af9871
kharisma-aw <aw.kharisma@gmail.com>
2020-03-18 13:15:25 +0700

    move UserDetailsServiceImpl to service package

--- src/main/kotlin/com/onboarding/userland/service/UserDetailsServiceImpl.kt
+++ src/main/kotlin/com/onboarding/userland/service/UserDetailsServiceImpl.kt
@@ -1,4 +1,4 @@
-package com.onboarding.userland.security
+package com.onboarding.userland.service
 
 import com.onboarding.userland.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
@@ -6,7 +6,9 @@ import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Component
 
+@Component
 class UserDetailsServiceImpl @Autowired constructor(private val repository: UserRepository) : UserDetailsService {
     override fun loadUserByUsername(email: String): UserDetails {
         val user = repository.findByEmail(email) ?: throw UsernameNotFoundException(email)
