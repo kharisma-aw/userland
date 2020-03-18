3008f55524fe6aa3c51f0013be69f0aa21a24124
kharisma-aw <aw.kharisma@gmail.com>
2020-03-18 13:22:00 +0700

    remove bean functions in app class

--- src/main/kotlin/com/onboarding/userland/UserlandApplication.kt
+++ src/main/kotlin/com/onboarding/userland/UserlandApplication.kt
@@ -1,35 +1,13 @@
 package com.onboarding.userland
 
-import com.onboarding.userland.repository.UserRepository
-import com.onboarding.userland.security.UserDetailsServiceImpl
-import com.onboarding.userland.service.AuthenticationService
-import com.onboarding.userland.service.UserDetailService
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession
 
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = [SecurityAutoConfiguration::class])
-class UserlandApplication {
-	@Bean
-	fun authService(repository: UserRepository, encoder: PasswordEncoder): AuthenticationService {
-		return AuthenticationService(repository, encoder)
-	}
-
-	@Bean
-	fun userDetailService(repository: UserRepository): UserDetailService {
-		return UserDetailService(repository)
-	}
-
-	@Bean
-	fun userDetailsServiceImpl(repository: UserRepository): UserDetailsServiceImpl {
-		return UserDetailsServiceImpl(repository)
-	}
-}
+@EnableJdbcHttpSession
+class UserlandApplication
 
 fun main(args: Array<String>) {
 	runApplication<UserlandApplication>(*args)
